c44d6678baaaa77e8cf71117ce8c383b656d7915
BMN1987 <110631069+Baxtiyor87@users.noreply.github.com>
2022-10-30 16:01:04 +0500

    pdpui1

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,36 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration:     BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+              colors: [
+                Colors.green.shade900,
+                Colors.green.shade500,
+                Colors.green.shade400
+
+              ])
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:  [
+            const SizedBox(height: 80,),
+            // #login, #welcome
+            Padding(padding: const EdgeInsets.all(20),
+            child: Column(
+              children: const [
+                Text('Login', style: TextStyle(color: Colors.white, fontSize: 40),),
+                SizedBox(height: 10,),
+                Text('Welcome Back', style: TextStyle(color: Colors.white, fontSize: 20),)
+              ],
+            ),)
+
+          ],
+        ),
+      ),
+    );
   }
 }
